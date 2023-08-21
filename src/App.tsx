c9cea56bbaad8e50980ff9c6200d3ea6556a1736
hetx19 <hetsavaliya19@gmail.com>
2023-08-21 13:57:36 +0530

    Adding Styling to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+// Css
+import "./scss/index.scss";
+
+// Components
+import Background from "./components/Background";
+import Loader from "./components/Loader";
+
+// Sections
+import Navbar from "./sections/Navbar";
+import Footer from "./sections/Footer";
+
+// Pages
+const Search = lazy(() => import("./pages/Search"));
+const MyList = lazy(() => import("./pages/MyList"));
+const About = lazy(() => import("./pages/About"));
+const Compare = lazy(() => import("./pages/Compare"));
+const Pokemon = lazy(() => import("./pages/Pokemon"));
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="main-container">
+      <Background />
+      <Router>
+        <Suspense fallback={<Loader />}>
+          <div className="app">
+            <Navbar />
+            <Routes>
+              <Route element={<Search />} path="/search" />
+              <Route element={<MyList />} path="/list" />
+              <Route element={<About />} path="/about" />
+              <Route element={<Compare />} path="/compare" />
+              <Route element={<Pokemon />} path="/pokemon/:id" />
+              <Route element={<Navigate to="/pokemon/1" />} path="*" />
+            </Routes>
+            <Footer />
+          </div>
+        </Suspense>
+      </Router>
+    </div>
+  );
 }
