5037bf6e55f5e15e2d8c72b26430e1157d48ae17
hetx19 <hetsavaliya19@gmail.com>
2023-08-21 11:11:48 +0530

    Base Setup

---
+++ src/pages/MyList.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Wrapper from "../sections/Wrapper";
+
+function MyList() {
+  return <div>My List</div>;
+}
+
+export default Wrapper(MyList);
---
+++ src/pages/Pokemon/CapableMoves.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CapableMoves() {
+  return <div>Capable Moves</div>;
+}
+
+export default CapableMoves;
